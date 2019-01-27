13d976eb6b00fa8b385d9f2cc9a9ea6b6aabb5b1
danieltsaicw <45779735+danieltsaicw@users.noreply.github.com>
2019-01-27 12:41:14 +0800

    Create django_delploy.sh

---
+++ django_delploy.sh
@@ -0,0 +1,34 @@
+
+#EC2 shell
+sudo apt-get install python-pip 
+sudo apt-get install python-dev
+sudo apt-get install nginx 
+sudo apt-get install git
+sudo apt-get update
+
+sudo pip install virtualenv
+git clone https://github.com/<user>/<repo>.git
+cd <repo>
+virtualenv venv
+source venv/bin/activate
+pip install -r requirement.txt
+pip install django bcrypt django-extenstions
+pip install gunicorn
+cd <repo>
+sudo vim settings.py
+
+#settings.py
+ALLOWED_HOST = ['<XXX.XXX.XXX.XXX>']
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+
+#EC2 shell
+cd ..
+python manage.py collectstatic
+gunicorn --bind 0.0.0.0:8000 <zillow.wsgi:application>
+
+
+
+
+
+
+
